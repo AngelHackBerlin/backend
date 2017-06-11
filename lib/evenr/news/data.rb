d3159c01e58cddf603e4b858d7692bec9535939b
Kamil Lelonek <squixy.sln@gmail.com>
2017-06-11 15:32:23 +0200

    Change data

--- lib/evenr/news/data.rb
+++ lib/evenr/news/data.rb
@@ -4,64 +4,22 @@ module Evenr::News::Data
   def sample
     [
       {
-        description: "Diego Armando Maradona",
-        scoreTeam1:  0,
-        scoreTeam2:  0,
-        kind:        3,
-      },
-      {
-        description: "Christiano Ronaldo",
+        description: "Goooooal! Robert Lewandowski",
         scoreTeam1:  1,
-        scoreTeam2:  0,
+        scoreTeam2:  2,
         kind:        1,
       },
       {
-        description: "Pele <=> Alexis Sanchez",
+        description: "Pele substitute Alexis Sanchez",
         scoreTeam1:  1,
-        scoreTeam2:  0,
+        scoreTeam2:  2,
         kind:        2,
       },
       {
-        description: "Robert Lewandowski",
+        description: "Yellow card for Christiano Ronaldo",
         scoreTeam1:  1,
-        scoreTeam2:  1,
-        kind:        1,
-      },
-      {
-        description: "Gareth Bale",
-        scoreTeam1:  1,
-        scoreTeam2:  1,
-        kind:        3,
-      },
-      {
-        description: "Neymar",
-        scoreTeam1:  1,
-        scoreTeam2:  2,
-        kind:        1,
-      },
-      {
-        description: "Lionel Messi",
-        scoreTeam1:  1,
-        scoreTeam2:  2,
-        kind:        4,
-      },
-      {
-        description: "Luis Suarez",
-        scoreTeam1:  2,
-        scoreTeam2:  2,
-        kind:        1,
-      },
-      {
-        description: "Thomas Mueller",
-        scoreTeam1:  2,
         scoreTeam2:  2,
         kind:        3,
-      },
-      {
-        description: "Antoine Griezmann <=> Riyad Mahrez",
-        scoreTeam1:  2,
-        scoreTeam2:  2,
-        kind:        2,
       },
     ]
   end
